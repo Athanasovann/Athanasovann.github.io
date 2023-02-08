5ec3a2557133ab199ff422713b970d11d7141b8c
Athanasovann <vvs1261@ppsbstudent.org>
2023-02-08 03:56:41 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -59,9 +59,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay)
-    createCannon("right", 595, 3000);
-    createCannon("bottom", 800, 2500);
-
+    createCannon('right', 595, 3000);
+    createCannon('bottom', 800, 2500);
+    createCannon('left', 200, 2500);
 
 
 
